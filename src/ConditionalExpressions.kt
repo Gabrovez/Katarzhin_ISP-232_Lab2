28d8c79cbb1f80e49970e5b1c9a2faa713eb62ea
Gabrovez <gabrovez321@gmail.com>
2025-09-14 15:24:36 +0300

    Vip

---
+++ src/ConditionalExpressions.kt
@@ -0,0 +1,11 @@
+fun main(){
+    print("Введите ваш уровень: ")
+    val level = readln().toInt()
+    print("У вас есть VIP-статус? (true/false): ")
+    val hasVIP = readln().toBoolean()
+    if ((level >= 30 && hasVIP) || level >= 50) {
+        println("Доступ разрешен!")
+    } else {
+        println("Доступ запрещен!")
+    }
+}
