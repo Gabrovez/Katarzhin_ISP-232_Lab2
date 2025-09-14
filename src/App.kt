be4a72871ad004d7502a7fa5fa91c21907783afe
Gabrovez <gabrovez321@gmail.com>
2025-09-14 17:23:20 +0300

    //Задание 2

--- src/App.kt
+++ src/App.kt
@@ -1,6 +1,6 @@
 fun main(){
     //Задание 1
-    print("Введите ваш уровень: ")
+    /*print("Введите ваш уровень: ")
     val level = readln().toInt()
     if (level <10){
         println("Новичок! Вы получаете +10 HP.")
@@ -8,5 +8,15 @@ fun main(){
         println("Опытный! Вы получаете +30 HP.")
     }else {
         println("Легенда! Вы получаете +100 HP.")
+    }*/
+    //Задание 2
+    print("Введите ваш возраст: ")
+    val level = readln().toInt()
+    if (level <12){
+        println("Вы ребёнок")
+    }else if (level < 17){
+        println("Вы подросток")
+    }else {
+        println("Вы взрослый")
     }
 }
