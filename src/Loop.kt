d49947bb32f337930f636571718cff16bb979946
Gabrovez <gabrovez321@gmail.com>
2025-10-03 17:01:26 +0300

    Шаг 8. Введение в коллекции

--- src/Loop.kt
+++ src/Loop.kt
@@ -1,5 +1,5 @@
 fun main(){
-    println("Начинаем ритуал защиты!")
+    /*println("Начинаем ритуал защиты!")
     repeat(5){index ->
         if (index == 2 ){
             println("Пропускаем шаг ${index + 1} из-за магического вмещательства!")
@@ -7,5 +7,11 @@ fun main(){
         }
         println("Заклинание защиты активировано!(Шаг ${index + 1})")
     }
-    println("Замок защищен!")
+    println("Замок защищен!")*/
+    val months = listOf("Python", "Kotlin", "C#", "C++", "Java")
+    println(months[0])
+    println(months.size)
+    println("Kotlin" in months)
+
+
 }
