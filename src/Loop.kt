d56aafe04113c7821e936fe6eb707cda86f337e9
Gabrovez <gabrovez321@gmail.com>
2025-10-03 17:06:41 +0300

    Пользователь вводит номер месяца (от 1 до 12), а программа выводит количество дней в этом месяце. Без високосного года.

--- src/Loop.kt
+++ src/Loop.kt
@@ -8,10 +8,20 @@ fun main(){
         println("Заклинание защиты активировано!(Шаг ${index + 1})")
     }
     println("Замок защищен!")*/
-    val months = listOf("Python", "Kotlin", "C#", "C++", "Java")
+    /*val months = listOf("Python", "Kotlin", "C#", "C++", "Java")
     println(months[0])
     println(months.size)
-    println("Kotlin" in months)
+    println("Kotlin" in months)*/
+    val daysInMonth = listOf(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+    print("Введите номер месяца (1-12): ")
+    val input = readln().toInt()
+    if (input in 1..12){
+        val days = daysInMonth[input -1]
+        println("В этом месяце $days дней.")
+    } else {
+        println("Некорректный ввод месяца.")
+    }
+
 
 
 }
