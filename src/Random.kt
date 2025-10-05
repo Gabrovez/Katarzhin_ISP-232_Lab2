4621d816878fa0dd70426e7be46fabd0e52bf2dc
Gabrovez <gabrovez321@gmail.com>
2025-10-05 10:10:00 +0300

    Шаг 8. Random 2

--- src/Random.kt
+++ src/Random.kt
@@ -1,6 +1,6 @@
 import kotlin.random.Random
 fun main() {
-    val target = Random.nextInt(1,100)
+    /*val target = Random.nextInt(1,100)
     println("Угадай число от 1 до 100")
     while (true){
         print("Введи число: ")
@@ -14,5 +14,12 @@ fun main() {
             }
 
         }
-    }
+    }*/
+    val d1 = Random.nextInt(1,7)
+    val d2 = Random.nextInt(1,7)
+    val sum = d1 + d2
+    println("Первый кубик: $d1")
+    println("Второй кубик: $d2")
+    println("Сумма: $sum")
+
 }
