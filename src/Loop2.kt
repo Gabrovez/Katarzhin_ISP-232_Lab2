447892925789fab86b685493baa22f87319282c7
Gabrovez <gabrovez321@gmail.com>
2025-10-05 08:09:57 +0300

    Шаг 2. Операторы continue и break

---
+++ src/Loop2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var number = 0
+    while (number < 10){
+        number++
+        if (number == 3){
+            println("Останавливаемся на $number")
+            continue
+        }
+        println(number)
+    }
+}
